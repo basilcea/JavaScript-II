77fa346ceaf00f1d789141b79474a1ef836c9a6d
cea <ogbonnabasil@Ogbonnas-MacBook-Pro.local>
2019-05-07 12:52:19 +0100

    write callbacks for various functions

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -23,33 +23,48 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
   });
 
 */
-
+const callback = (value) =>{
+  console.log(value)
+}
 
 function getLength(arr, cb) {
+  return cb(arr.length)
   // getLength passes the length of the array into the callback.
 }
+getLength(items, callback)
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr.pop())
 }
+last(items, callback)
+
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y)
 }
+sumNums(1, 2, callback)
 
 function multiplyNums(x, y, cb) {
+  return cb(x * y)
   // multiplyNums multiplies two numbers and passes the result to the callback.
 }
+multiplyNums(1,2,callback)
 
 function contains(item, list, cb) {
+    return cb(list.includes(item))
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
 }
+contains(5, [1,2,3], callback)
 
 /* STRETCH PROBLEM */
 
 function removeDuplicates(array, cb) {
+  return cb([...new Set(array)])
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
 }
+removeDuplicates([1,1,3,2,2,4], callback)
